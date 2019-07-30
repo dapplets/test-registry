73957e85cc678888f50389ac307a74ffbd4463dc
Alexander Sakhaev <alsakhaev@gmail.com>
2019-07-30 15:38:38 +0300

    fix data storage

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,12 @@ import { DATA_PATH } from "./common/constants";
 if (!fs.existsSync(DATA_PATH)) {
     fs.mkdirSync(DATA_PATH);
 }
+if (!fs.existsSync(DATA_PATH + '/storage')) {
+    fs.mkdirSync(DATA_PATH + '/storage');
+}
+if (!fs.existsSync(DATA_PATH + '/accounts')) {
+    fs.mkdirSync(DATA_PATH + '/accounts');
+}
 
 var app = express();
 app.use(bodyParser.urlencoded());
