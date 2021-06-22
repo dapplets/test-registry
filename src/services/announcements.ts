1cb7f00e99a5a624ffb3fde754da8e0d2bee401c
Alexander Sakhaev <alsakhaev@gmail.com>
2021-06-22 16:16:54 +0300

    improvement: changed announcements limit from 10 to 12

--- src/services/announcements.ts
+++ src/services/announcements.ts
@@ -4,7 +4,7 @@ export async function getAnnouncements() {
     const client = new Client();
     await client.login(process.env.DISCORD_BOT_KEY as string);
     const channel = await client.channels.fetch(process.env.DISCORD_CHANNEL_ID as string) as any;
-    const rawMessages = await channel.messages.fetch({ limit: 10 }) as any[];
+    const rawMessages = await channel.messages.fetch({ limit: 12 }) as any[];
     
     const messages = rawMessages.map((x) => ({
         authorUsername: x.author.username,
