c8baaea256680b09676d785d27702afa063fa74d
Alexander Sakhaev <alsakhaev@gmail.com>
2021-06-29 12:54:41 +0300

    feat: filter messages by "ALERT" keyword

--- src/services/announcements.ts
+++ src/services/announcements.ts
@@ -5,13 +5,15 @@ export async function getAnnouncements() {
     await client.login(process.env.DISCORD_BOT_KEY as string);
     const channel = await client.channels.fetch(process.env.DISCORD_CHANNEL_ID as string) as any;
     const rawMessages = await channel.messages.fetch({ limit: 12 }) as any[];
-    
-    const messages = rawMessages.map((x) => ({
-        authorUsername: x.author.username,
-        content: x.content, 
-        timestamp: new Date(x.createdTimestamp).toISOString(),
-        link: `https://discord.com/channels/${x.channel.guild.id}/${x.channel.id}/${x.id}`
-    }));
+
+    const messages = rawMessages
+        .filter(x => x.content.indexOf('ALERT') === 0)
+        .map((x) => ({
+            authorUsername: x.author.username,
+            content: x.content.replace('ALERT', '').trim(),
+            timestamp: new Date(x.createdTimestamp).toISOString(),
+            link: `https://discord.com/channels/${x.channel.guild.id}/${x.channel.id}/${x.id}`
+        }));
 
     return messages;
 }
